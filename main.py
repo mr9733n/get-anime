946ddbef645ca50eafb12d071a6c64237fcaf299
mr9733n <128349570+mr9733n@users.noreply.github.com>
2024-11-01 03:10:45 +0200

    3.0.1

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 # main.py
-
 import logging.config
 import tkinter as tk
 from core.app import AnimePlayerApp
+from core.database_manager import DatabaseManager  # Импорт DatabaseManager
 
 if __name__ == "__main__":
     logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
 
+    # Создаем и инициализируем таблицы базы данных
+    db_manager = DatabaseManager()
+    db_manager.initialize_tables()
+
     window = tk.Tk()
-    app = AnimePlayerApp(window)
+    app = AnimePlayerApp(window, db_manager)  # Передаем db_manager в приложение
     window.mainloop()

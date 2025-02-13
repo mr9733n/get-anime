d3d448c2a9c6013a49833ab014cd3d785f497126
mr9733n <128349570+mr9733n@users.noreply.github.com>
2025-02-14 01:37:37 +0200

    0.3.8.13

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 import re
 import subprocess
 import sys
-import argparse
 import threading
 
 from PyQt5.QtCore import QSharedMemory
@@ -13,17 +12,25 @@ from PyQt5.uic.Compiler.qobjectcreator import logger
 from core.database_manager import DatabaseManager
 from app.qt.app import AnimePlayerAppVer3
 from dotenv import load_dotenv
+from utils.library_loader import verify_library, load_library
 
 
 APP_MINOR_VERSION = '0.3.8'
 APP_MAJOR_VERSION = '0.3'
 
 load_dotenv()
+prod_key = os.getenv("PROD_KEY")
+fetch_ver = os.getenv('USE_GIT_VERSION')
+
+UUID_REGEX = r'^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$'
+if prod_key and re.match(UUID_REGEX, prod_key):
+    DEVELOPMENT_MODE = False
+else:
+    DEVELOPMENT_MODE = True
 
 def fetch_version():
     global version
-    # development
-    if os.getenv('USE_GIT_VERSION') == '0':
+    if DEVELOPMENT_MODE and fetch_ver == '1':
         try:
             commit_message = subprocess.check_output(['git', 'log', '-1', '--pretty=%B'], text=True).strip()
             version_pattern = r"^\d+\.\d+\.\d+\.\d+$"
@@ -37,7 +44,6 @@ def fetch_version():
             logger.error(f"Error occurred while getting commit message: {e}")
             version = APP_MAJOR_VERSION
     else:
-        # production
         version = APP_MINOR_VERSION
         logger.info(f"Production version: {version}")
 
@@ -49,40 +55,52 @@ if __name__ == "__main__":
 
     logging.config.fileConfig('config/logging.conf', disable_existing_loggers=False)
 
-    # Создаём объект разделяемой памяти с уникальным ключом
-    unique_key = "Xokl-xhcL-moUl-xz04-Kg5v-VtL9-3IQH-UOV7"
-    shared_memory = QSharedMemory(unique_key)
-
-    # Пытаемся создать сегмент памяти размером в 1 байт
-    if not shared_memory.create(1):
-        logger.error("App is already running!")
-        sys.exit(1)
-
     # Construct the path to the database in the main directory
     base_dir = os.path.dirname(os.path.abspath(__file__))
     db_dir = os.path.join(base_dir, 'db')
     icon_dir = os.path.join(base_dir, 'static')
+    lib_dir = os.path.join(base_dir, 'libs')
+
+    # Create object with unique key
+    if not DEVELOPMENT_MODE:
+        unique_key = prod_key  # use key generated on build
+        shared_memory = QSharedMemory(unique_key)
+        if not shared_memory.create(1):
+            logging.getLogger(__name__).error("App is already running!")
+            sys.exit(1)
+    else:
+        logging.getLogger(__name__).info("Development mode: single instance check disabled.")
+
+    # Check vlc library
+    try:
+        expected_hash = "a2625d21b2cbca52ae5a9799e375529c715dba797a5646adf62f1c0289dbfb68"
+        lib_file_path = load_library(lib_dir, 'libvlc.dll')
+        verify_library(lib_file_path, expected_hash)
+    except Exception as e:
+        logger.error(f"Failed to initialize library: {e}")
+        exit(1)
+
     # Ensure the database directory exists
     if not os.path.exists(db_dir):
         os.makedirs(db_dir)
 
     db_path = os.path.join(db_dir, 'anime_player.db')
 
-    # Создаем и инициализируем таблицы базы данных
+    # Create database on first start
     db_manager = DatabaseManager(db_path)
     db_manager.initialize_tables()
     db_manager.save_template(template_name='default')
     db_manager.save_placeholders()
 
-    # Запускаем поток для получения версии
+    # Check version
     version_thread = threading.Thread(target=fetch_version)
     version_thread.start()
-    # Ждем, пока поток завершится, прежде чем показывать интерфейс
+    # finishing version check
     version_thread.join()
-    # Пока версия загружается, можем начать работу интерфейса
+    # Starting application window
     app_pyqt = QApplication(sys.argv)
 
-    icon_path = os.path.join(icon_dir, 'icon.png')  # Путь к файлу с иконкой
+    icon_path = os.path.join(icon_dir, 'icon.png')
     app_pyqt.setWindowIcon(QIcon(icon_path))
     window_pyqt = AnimePlayerAppVer3(db_manager, version)
     window_pyqt.show()
